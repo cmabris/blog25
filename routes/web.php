2a46d204bce802d5e59191eadbc9733851c5e5fe
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-01 16:49:10 +0200

    Paso 1

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+Route::view('contacto', 'contact');
+Route::view('blog', 'blog');
+Route::view('nosotros', 'about');
+
+//Uso del método Match para responder a múltiples verbos HTTP
+Route::match(['put', 'patch'], 'match', function () {
+    return 'Hola desde la ruta match';
+});
+
+Route::any('any', function () {
+    //
 });
