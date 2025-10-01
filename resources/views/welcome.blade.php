299df0c74f761e2cedddb3a4054881eca5131464
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-01 17:09:53 +0200

    Paso 3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,7 @@
     <title>Inicio</title>
 </head>
 <body>
-<ul>
-    <li><a href="<?= route('home') ?>">Home</a></li>
-    <li><a href="<?= route('blog') ?>">Blog</a></li>
-    <li><a href="<?= route('about') ?>">Nosotros</a></li>
-    <li><a href="<?= route('contact') ?>">Contacto</a></li>
-</ul>
+@include('partials.navigation')
 <h1>Inicio</h1>
 </body>
 </html>
