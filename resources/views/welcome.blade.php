4eeaad87feb8552c935ed234c64b77872f1bc7f9
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-03 19:32:21 +0200

    Paso 4

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0,
-          maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Inicio</title>
-</head>
-<body>
-@include('partials.navigation')
-<h1>Inicio</h1>
-</body>
-</html>
+<x-layout meta-title="Inicio" meta-description="Descripción de la página de inicio">
+    <h1>Inicio</h1>
+    <x-slot:sidebar>
+        <p>Home Sidebar</p>
+    </x-slot:sidebar>
+</x-layout>
