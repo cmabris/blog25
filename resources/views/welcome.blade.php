4728875a395d00eddbd78a4a77bd31d9f3e171ac
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-17 20:53:44 +0200

    Paso 22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,7 @@
 <x-layout meta-title="Inicio" meta-description="Descripción de la página de inicio">
-    <h1>Inicio</h1>
-    <x-slot:sidebar>
-        <p>Home Sidebar</p>
-    </x-slot:sidebar>
+    <div class="mx-auto mt-4 max-w-6xl">
+        <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
+            Inicio
+        </h1>
+    </div>
 </x-layout>
