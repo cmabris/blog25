24db1ac6b25a81bdb50161ac5b178878fb39facf
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2025-10-24 18:58:47 +0200

    Paso 28

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,4 +1,4 @@
-<x-layout meta-title="Contacto" meta-description="Descripción de la página de Contacto">
+<x-blog-layout meta-title="Contacto" meta-description="Descripción de la página de Contacto">
     <div class="mx-auto mt-4 max-w-6xl">
         <h1 class="mt-4 mb-8 text-center font-serif text-4xl font-extrabold text-sky-600 md:text-5xl">
             Contacto
